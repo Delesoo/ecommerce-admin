cf3ea5a81e63809d511ad97ab5314361939ce675
Delesoo <strongtoka@gmail.com>
2024-01-18 14:49:09 +0400

    minor changes

--- pages/orders.js
+++ pages/orders.js
@@ -16,6 +16,7 @@ export default function OrdersPage() {
                 <thead>
                     <tr>
                         <th>Date</th>
+                        <th>Paid</th>
                         <th>Recipient</th>
                         <th>Products</th>
                     </tr>
@@ -24,6 +25,9 @@ export default function OrdersPage() {
                     {orders.length > 0 && orders.map(order => (
                         <tr>
                             <td>{(new Date(order.createdAt)).toLocaleString()}</td>
+                            <td className={order.paid ? 'text-green-600' : 'text-red-600'}>
+                                {order.paid ? 'YES' : "NO"}
+                            </td>
                             <td>
                                 {order.name} {order.email}<br />
                                 {order.city} {order.postalCode} {order.country}<br />
